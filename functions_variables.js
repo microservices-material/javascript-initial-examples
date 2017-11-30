09b02e743e95a648278c4792f2bd5f605d84d214
Carlos Lombardi <carlombardi@gmail.com>
2017-11-30 17:45:41 +0000

    first source file with basic examples

---
+++ functions_variables.js
@@ -0,0 +1,15 @@
+function max(n1,n2) {
+  if (n1 >= n2) {
+    return n1
+  } else {
+    return n2
+  }
+}
+
+function triple(n) { return n * 3 }
+
+a = max(3,4)
+b = max(a, 28)
+c = triple(9)
+
+theArray = [a,b]
